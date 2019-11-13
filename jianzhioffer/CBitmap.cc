92c47ed18c4a0189d1308d8716d5db72eca3d058
Ada <771243335@qq.com>
2019-11-13 17:17:27 +0800

    Add files via upload

--- jianzhioffer/CBitmap.cc
+++ jianzhioffer/CBitmap.cc
@@ -4,8 +4,9 @@ private:
 	const static unsigned int UINT_BITS = sizeof(unsigned int);
 	const static unsigned int SHIFT = UINT_BITS +1;
 	const static unsigned int MASK = 0x1F;
-	const static unsigned int MAX_NUMBER_VALUE = 1024 * 1024 * 1024;//10亿
-		  static unsigned int ARR[MAX_NUMBER_VALUE / UINT_BITS];//可以存放 MAX_NUMBER_VALUE / UINT_BITS * 8(bit)
+	const static unsigned int MAX_NUMBER_VALUE = (long long)(1024ll * 1024ll * 1024ll * 4ll) -1;//40亿 B、KB、MB、GB。
+	const static long long count = ((long long)MAX_NUMBER_VALUE + 1) / UINT_BITS / 8;
+		  static unsigned int ARR[count];// MAX_NUMBER_VALUE / UINT_BITS个int的数组。  可以存放 MAX_NUMBER_VALUE / UINT_BITS * 8(bit)
 
 public:
 
@@ -19,6 +20,57 @@ public:
 	}
 
 	void clear(unsigned int i) {
-		ARR[i >> SHIFT] & ~(1 << (i & MASK));
+		ARR[i >> SHIFT] &= (~(1 << (i & MASK)));
 	}
 };
+
+#include <string.h>
+#include <vector>
+
+void all_range(std::string str, int k) {
+
+
+	for (int i = k; i < str.length(); ++i) {
+		char tmp = str[i];
+		str[i] = str[k];
+		str[k] = tmp;
+
+		all_range(str,  k + 1);
+
+		tmp = str[k];
+		str[k] = str[i];
+		str[i] = tmp;
+		
+	}
+
+	if (k == str.length())
+		fprintf(stdout, "%s\n", str.c_str());
+}
+
+#include <unordered_map>
+
+int main(int argc, char** argv) {
+
+
+	int a = sizeof(int);
+	int b = sizeof(long);
+	int c = sizeof(long long);
+	CBitmap cb;
+
+	char str[] = "abc";
+	int len = strlen(str);
+	char str1[10] = { 0x01 };
+	memset(str1, 0x01, sizeof str1);
+	strcpy(str1, str);
+
+
+
+	all_range(str, 0);
+
+
+
+
+	
+
+	return 0;
+}
